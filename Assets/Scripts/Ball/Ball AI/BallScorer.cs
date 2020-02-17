ea6622a23a2e26c4a7960b4a5a553a2f90f71cb4
jay <jay@raccoopack.com>
2020-02-17 15:54:56 -0800

    Reverted Apex and then installed Panda.

---
+++ Assets/Scripts/Ball/Ball AI/BallScorer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JFrisoGames.PuffMan
+{
+    public class BallScorer : MonoBehaviour
+    {    
+        /******* Variables & Properties*******/
+        
+        /******* Monobehavior Methods *******/
+
+        /******* Methods *******/
+    }
+}
